b06a7b9e8ccea924346804a38725b18e74e87752
Leo24 <leo2410@yandex.ru>
2017-04-21 09:25:33 +0300

    Add auth and routes

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Admin
+{
+
+    const ADMIN_LABEL = 'ADMIN';
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,73 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+Route::get('/logout', 'Auth\LoginController@logout')->name('admin.logout');
+
+Route::group(['middleware' => ['web']], function() {
+
+    // Site routes
+    Route::get('/', 'SerialController@index')->name('site.index');
+
+
+
+    Route::group(['middleware' => ['auth', 'admin'], 'prefix' => 'admin'], function () {
+        Route::get('/', 'AdminController@index')->name('admin.index');
+
+
+        /**
+         * Serials
+         */
+
+        Route::get('serials', 'SerialController@index')->name('admin.serial.index');
+        Route::any('serials/create', 'SerialController@create')->name('admin.serial.create');
+        Route::post('serial/{serial}/update', 'SerialController@update')->name('admin.serial.update');
+        Route::get('serial/{serial}/edit', 'SerialController@edit')->name('admin.serial.edit');
+        Route::post('serial/{serial}/delete', 'SerialController@delete')->name('admin.serial.delete');
+        Route::get('serial/{serial}/remove-picture', 'SerialController@removePicture')->name('serial.remove.picture');
+
+
+
+        /**
+         * Seasons
+         */
+
+        Route::get('seasons', 'SeasonController@index')->name('admin.season.index');
+        Route::any('season/create', 'SeasonController@create')->name('admin.season.create');
+        Route::post('season/{season}/update', 'SeasonController@update')->name('admin.season.update');
+        Route::get('season/{season}/edit', 'SeasonController@edit')->name('admin.season.edit');
+        Route::post('season/{season}/delete', 'SeasonController@delete')->name('admin.season.delete');
+        Route::get('season/{season}/remove-picture', 'SeasonController@removePicture')->name('season.remove.picture');
+        Route::post('season/search', 'SeasonController@search')->name('seasons.search');
+
+
+        /**
+         * Episodes
+         */
+
+        Route::get('episodes', 'EpisodeController@index')->name('admin.episode.index');
+        Route::any('episode/create', 'EEisodeController@create')->name('admin.episode.create');
+        Route::post('episode/{episode}/update', 'EpisodeController@update')->name('admin.episode.update');
+        Route::get('episode/{episode}/edit', 'EpisodeController@edit')->name('admin.episode.edit');
+        Route::post('episode/{episode}/delete', 'EpisodeController@delete')->name('admin.episode.delete');
+
+        
+        /**
+         * Genres
+         */
+
+        Route::get('genres', 'GenresController@index')->name('admin.genres.index');
+        Route::any('genre/create', 'GenreController@create')->name('admin.genre.create');
+        Route::post('genre/{genre}/update', 'GenreController@update')->name('admin.genre.update');
+        Route::get('genre/{genre}/edit', 'GenreController@edit')->name('admin.genre.edit');
+        Route::post('genre/{genre}/delete', 'GenreController@delete')->name('admin.genre.delete');
+        Route::get('genre/{genre}/check', 'GenreController@check')->name('admin.genre.check');
+
+    });
+
 });
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+
+
+
